db7cbfbff55bf05d5627d32704593f12d2940b1d
Gehu <lauric_gehu@etu.u-bourgogne.fr>
2025-05-20 09:44:34 +0200

    crate version

--- publish_one.sh
+++ publish_one.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/expect -f
 set CRATE_NAME [lindex $argv 0]
+set CRATE_VERSION "0.1.0-dev"
 
 spawn alr --force publish --tar --skip-build --for-private-index
 expect "Do you want to proceed with this information?"
 send -- "Y\n"
 expect "Enter URL"
-send -- "https://github.com/lgehu/alr_adl_crates/raw/refs/heads/remote-dep/$CRATE_NAME/$CRATE_NAME-0.1.0-dev.tgz\n"
+send -- "https://github.com/lgehu/alr_adl_crates/raw/refs/heads/remote-dep/$CRATE_NAME/$CRATE_NAME-$CRATE_VERSION.tgz\n"
 expect "Is this information correct?"
 send -- "Y\n"
 expect "Do you want to proceed with this information?"
