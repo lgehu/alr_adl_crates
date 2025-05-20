b9f15afe9057dbfa387ef9b09d87a3b80c14f1a1
Gehu <lauric_gehu@etu.u-bourgogne.fr>
2025-05-20 14:39:59 +0200

    arg for indivual crate

--- publish_all.sh
+++ publish_all.sh
@@ -4,8 +4,14 @@
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 CRATE_VERSION="0.1.0-dev"
 
+if [[ $# > 0 ]] then
+    CRATES=("$@")
+else
+    CRATES="*/"
+fi
+
 # Generate archives and manifest
-for dir in */ ; do
+for dir in $CRATES; do
     CRATE_NAME=${dir%/}
     PREFIX=${CRATE_NAME:0:2}
     (
@@ -15,7 +21,7 @@ for dir in */ ; do
 done
 
 # Copy manifest to alire index
-for dir in */ ; do
+for dir in $CRATES; do
     CRATE_NAME=${dir%/}
     PREFIX=${CRATE_NAME:0:2}
 
@@ -24,7 +30,7 @@ for dir in */ ; do
     cp $CRATE_NAME/alire/archives/* $CRATE_NAME/
    
     # Replace local URL by remote URL
-    sed -i '/url =/c\url = https://github.com/lgehu/alr_adl_crates/raw/refs/heads/remote-dep/'$CRATE_NAME-$CRATE_VERSION.tgz $RELEASE_P/$CRATE_NAME-$CRATE_VERSION.toml
+    sed -i "/url =/c\url = \"https://github.com/lgehu/alr_adl_crates/raw/refs/heads/remote-dep/$CRATE_NAME-$CRATE_VERSION.tgz\"" $RELEASE_P/$CRATE_NAME-$CRATE_VERSION.toml
     cp $RELEASE_P/* ../alire-index/index/$PREFIX/$CRATE_NAME/
 done
 
