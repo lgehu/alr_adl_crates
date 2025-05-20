7bc30a5b8100a233a70f76308cf53f5b238a8c6f
Gehu <lauric_gehu@etu.u-bourgogne.fr>
2025-05-20 09:19:43 +0200

    second loop for cp

--- publish_all.sh
+++ publish_all.sh
@@ -3,11 +3,16 @@ SCRIPT_DIR=$(dirname "$(realpath "$0")")
 for dir in */ ; do
     CRATE_NAME=${dir%/}
     PREFIX=${CRATE_NAME:0:2}
-
     (
         cd "$CRATE_NAME" || exit
     "$SCRIPT_DIR/publish_one.sh" "$CRATE_NAME"
     )
+done
+
+SCRIPT_DIR=$(dirname "$(realpath "$0")") 
+for dir in */ ; do
+    CRATE_NAME=${dir%/}
+    PREFIX=${CRATE_NAME:0:2}
     cp $CRATE_NAME/alire/archives/* $CRATE_NAME/
     cp $CRATE_NAME/alire/releases/* ../alire-index/index/$PREFIX/$CRATE_NAME/
 done
