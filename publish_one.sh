c36e5e1ef39da29ea1d96558f0b50c2a9151b80a
Gehu <lauric_gehu@etu.u-bourgogne.fr>
2025-05-20 12:28:15 +0200

    changed remote URL to local path

--- publish_one.sh
+++ publish_one.sh
@@ -6,7 +6,7 @@ spawn alr --force publish --tar --skip-build --for-private-index
 expect "Do you want to proceed with this information?"
 send -- "Y\n"
 expect "Enter URL"
-send -- "https://github.com/lgehu/alr_adl_crates/raw/refs/heads/remote-dep/$CRATE_NAME/$CRATE_NAME-$CRATE_VERSION.tgz\n"
+send -- "alire/archives/$CRATE_NAME-$CRATE_VERSION.tgz\n"
 expect "Is this information correct?"
 send -- "Y\n"
 expect "Do you want to proceed with this information?"
